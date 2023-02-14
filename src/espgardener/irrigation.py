abf5352e56bb1fa51d057fc1fede2af228c27a33
MartinP1199 <potociarmartin@gmail.com>
2023-02-14 07:59:18 +0100

    update

--- src/espgardener/irrigation.py
+++ src/espgardener/irrigation.py
@@ -1,13 +1,16 @@
 from machine import Pin
 import time
 
+sprinkler = Pin(2, Pin.OUT)
+sprinkler.value(True)
+
 def irrigate(measurement):
     soil_hum = int(measurement["soil_hum"])
-    sprinkler = Pin(2, Pin.OUT)
     period = 0
     if soil_hum < 75:
-        sprinkler.value(True)
+        sprinkler.value(False)
         period = soil_hum * -0.1 + 10
         time.sleep(period)
-        sprinkler.value(False)
+        sprinkler.value(True)
     return period    
+ 
--- src/espgardener/measurement.py
+++ src/espgardener/measurement.py
@@ -3,9 +3,10 @@ import time
 from machine import ADC, Pin
 from time_utils import *
 
-global soil_detector
-soil_detector = ADC(Pin(36), atten=ADC.ATTN_11DB)
-    
+SOIL_HUM_MIN = 26
+SOIL_HUM_MAX = 51
+
+global soil_detector   
 def measure():
     
     d = dht.DHT22(Pin(4))
@@ -15,7 +16,7 @@ def measure():
     soil_detector = ADC(Pin(34))
     soil_detector.atten(ADC.ATTN_11DB)
     print(soil_detector.read())
-    value = soil_detector.read() // -40.96 +100
+    value = soil_detector.read() #// -40.96 +100
     measurement = {
         "air_temp":d.temperature(),
         "air_hum":d.humidity(),
